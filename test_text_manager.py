587370b48b371b24ad872433edb0014b34970a33
phil <phil@phil-ThinkPad-X60.(none)>
2012-09-16 00:46:12 -0500

    UDT:daily update.

--- test_text_manager.py
+++ test_text_manager.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python -B
+
 import text_manager
 tm = text_manager.TextManager()
 tm.out()
+tm.out()
+tm.out()
+tm.out()
+string = tm.out()
+print string 
--- text_manager.py
+++ text_manager.py
@@ -1,8 +1,16 @@
+import re
+
 class TextManager :
     def __init__(self) :
         self.f = open("in_text")
+        self.lines = self.f.readlines()
+        self.line_count = 0
 
     def out(self) :
-        lines = self.f.readlines()
-        print(lines) 
-
+        line_string = self.lines[self.line_count] 
+        if line_string != '\n' : 
+             print line_string
+        words = re.findall(r'\w+', line_string)
+        for word in words :
+            print word
+        self.line_count += 1
